6ba67cef3e0153845249bfc178447b7e37133001
Jammersmurph <holleranjameson34@gmail.com>
2026-07-25 15:15:30 -0400

    Add standalone onboard configuration

---
+++ aircraft/config.example.lua
@@ -0,0 +1,36 @@
+return {
+  updateInterval = 0.10,
+  telemetryPeripheral = nil,
+  monitorPeripheral = nil,
+  safety = {
+    maximumOutput = 5,
+    maximumRemotePulse = 2.0,
+    disengageOnTelemetryLoss = true,
+    clearOutputsOnExit = true,
+  },
+  navigation = {
+    cruiseSpeed = 12,
+    approachSpeed = 4,
+    slowdownRadius = 50,
+    arrivalRadius = 5,
+    stopSpeed = 0.5,
+  },
+  orientation = {
+    forward = { x = 0, y = 0, z = -1 },
+    up = { x = 0, y = 1, z = 0 },
+  },
+  network = {
+    enabled = false,
+    protocol = "cc-navtool",
+    host = "navtool-aircraft",
+    sharedKey = "",
+  },
+  outputs = {
+    forward = { side = "front", analog = true, inverted = false, maximum = 5 },
+    reverse = { side = "back", analog = true, inverted = false, maximum = 5 },
+    left = { side = "left", analog = true, inverted = false, maximum = 5 },
+    right = { side = "right", analog = true, inverted = false, maximum = 5 },
+    up = { side = "top", analog = true, inverted = false, maximum = 5 },
+    down = { side = "bottom", analog = true, inverted = false, maximum = 5 },
+  },
+}
