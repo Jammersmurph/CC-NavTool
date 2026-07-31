3960ba2cd6cac37dfdc833df24d6e579de075fac
Jameson Holleran <holleranjameson34@gmail.com>
2026-07-30 21:46:46 -0400

    Restore develop nightly version

--- aircraft/navtool.lua
+++ aircraft/navtool.lua
@@ -1,4 +1,4 @@
-local VERSION = "0.5.2"
+local VERSION = "0.5.2-nightly"
 local ROOT = "/navtool"
 local CONFIG_PATH = ROOT .. "/config.lua"
 local TARGET_PATH = ROOT .. "/target.db"
--- remote/controller.lua
+++ remote/controller.lua
@@ -1,4 +1,4 @@
-local VERSION = "0.5.2"
+local VERSION = "0.5.2-nightly"
 local ROOT = "/navremote"
 local CONFIG_PATH = ROOT .. "/config.lua"
 local Storage = dofile(ROOT .. "/storage.lua")
