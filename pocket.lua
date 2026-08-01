568de7341fabaa2c35bf0e3744a77043f53148d0
Jameson Holleran <holleranjameson34@gmail.com>
2026-07-31 23:30:34 -0400

    Release 0.5.3

--- pocket.lua
+++ pocket.lua
@@ -1,5 +1,5 @@
--- CC-NavTool wireless pocket remote v0.5.2-nightly
-local VERSION = "0.5.2-nightly"
+-- CC-NavTool wireless pocket remote v0.5.3
+local VERSION = "0.5.3"
 local CONFIG_PATH = "/navtool/remote.lua"
 local DEFAULT = {
   channel = "cc-navtool",
