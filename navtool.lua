8986f2a5b13adb8b7a8df1f3ca94ae44fa6a7102
Jameson Holleran <holleranjameson34@gmail.com>
2026-08-06 20:30:00 -0400

    Restore develop nightly after 0.5.10

--- navtool.lua
+++ navtool.lua
@@ -1,5 +1,5 @@
--- CC-NavTool v0.5.10
-local VERSION = "0.5.10"
+-- CC-NavTool v0.5.10-nightly
+local VERSION = "0.5.10-nightly"
 local ROOT = "/navtool"
 local CONFIG_PATH = ROOT .. "/config.lua"
 local STATE_PATH = ROOT .. "/state.lua"
--- pocket.lua
+++ pocket.lua
@@ -1,5 +1,5 @@
--- CC-NavTool wireless pocket remote v0.5.10
-local VERSION = "0.5.10"
+-- CC-NavTool wireless pocket remote v0.5.10-nightly
+local VERSION = "0.5.10-nightly"
 local CONFIG_PATH = "/navtool/remote.lua"
 local DEFAULT = {
   channel = "cc-navtool",
